15678876d375f4273cde72fd0db0fe65152cb766
sofiene <letaief.sofiene4@gmail.com>
2020-11-17 16:55:19 +0100

    initial commit

---
+++ AutoHire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoHire
+//
+//  Created by ESPRIT on 11/17/20.
+//  Copyright © 2020 ESPRIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
